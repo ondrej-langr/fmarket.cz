1759a87a6e7509992e752cfd81f3010515608c92
Ondřej Langr <hi@ondrejlangr.cz>
2022-11-27 15:59:11 +0100

    Add poppins font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,24 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+/** @type {import('tailwindcss/types/config').PluginCreator} */
+const projectPlugin = ({ addBase }) => {
+  
+}
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif', ...defaultTheme.fontFamily.sans]
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    projectPlugin
+  ],
 }
+
+
