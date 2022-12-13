9115ab9667cffbecf193762a6d368bde6b616afa
Ondřej Langr <hi@ondrejlangr.cz>
2022-12-13 10:28:04 +0100

    Update mobile

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,21 +1,21 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
-const tailwindScrollbar = require('tailwind-scrollbar');
+const tailwindScrollbar = require("tailwind-scrollbar");
 
 /** @type {import('tailwindcss/types/config').PluginCreator} */
 const projectPlugin = ({ addComponents }) => {
   addComponents({
-    '.text-column': {
-      maxWidth: 'none',
-      '@media and (min-width: 600px)': {
-        maxWidth: 460
-      }
-    }
-  })
+    ".text-column": {
+      maxWidth: "none",
+      "@media and (min-width: 600px)": {
+        maxWidth: 460,
+      },
+    },
+  });
 };
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx,astro}"],
   theme: {
     extend: {
       fontFamily: {
@@ -31,13 +31,24 @@ module.exports = {
       container: {
         center: true,
         padding: "2rem",
+
+        screens: {
+          sm: "600px",
+          md: "728px",
+          lg: "984px",
+          xl: "1240px",
+          "2xl": "1320px",
+        },
       },
       flex: {
         2: "2 1 0%",
       },
     },
   },
-  plugins: [projectPlugin, tailwindScrollbar({
-    nocompatible: true
-  })],
+  plugins: [
+    projectPlugin,
+    tailwindScrollbar({
+      nocompatible: true,
+    }),
+  ],
 };
